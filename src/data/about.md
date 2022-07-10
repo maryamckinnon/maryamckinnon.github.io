416a03ca495f166bd1ca9341727efefbe6ab5485
Mary McKinnon <your@email.heremaryamckinnon@gmail.com>
2022-07-10 11:07:48 -0700

    Made a few changes to resume and appearance

--- src/data/about.md
+++ src/data/about.md
@@ -9,7 +9,7 @@ I am a physical therapist turned software engineer and recent graduate of Hack R
 - Hiking
 - Meditation
 - Long walks exploring San Francisco
-- Puzzles of any kind
+- Puzzles of any kind (some favorites are Wordle/Octordle, jigsaw puzzles, and Soduko)
 - Cycling
 - Being near any large body of water
 - Podcasts ([Hidden Brain](https://www.npr.org/series/423302056/hidden-brain), [We Can Do Hard Things](http://wecandohardthingspodcast.com/), [The Heal Podcast](https://podcasts.apple.com/us/podcast/the-heal-podcast/id1562929417), [Radiolab](https://www.wnycstudios.org/shows/radiolab))
--- src/data/resume/positions.js
+++ src/data/resume/positions.js
@@ -6,7 +6,8 @@ const positions = [
     link: 'https://hackreactor.com',
     daterange: 'April 2022 - August 2022',
     points: [
-      'Accomplishments include but are not limited to becoming fluent in Python and JavaScript, building projects, learning Git navigation, utilizing Docker extensively, applying skills gained in postgreSQL and MongoDB, all while building numerous full-stack web applications using Django and React ',
+      'Developed multiple full-stack applications using Django and React by applying domain-driven design knowledge and adopting RESTful API framework',
+      'Used postgreSQL relational databases and MongoDB databases to store data ',
     ],
   },
   {
@@ -16,8 +17,8 @@ const positions = [
     link: 'https://healthwellpt.com/',
     daterange: 'January 2020 - April 2022',
     points: [
-      'Conducted evaluations, educated patients and/or caregivers on findings, created plans of care, and provided follow-up treatments',
-      'Attended multiple continuing education courses to stay informed of evidence-based practices and presented the information to clinic staff',
+      'Conducted evaluations, educated patients and/or caregivers on findings, created plans of care, and provided care',
+      'Attended continuing education courses to remain informed of best evidence-based practices and presented the information to clinic staff',
       'Discussed patient progress with top surgeons and specialists in the country to ensure comprehensive patient care',
     ],
   },
@@ -28,9 +29,8 @@ const positions = [
     link: 'https://www.jointventurespt.com/',
     daterange: 'May 2018 - November 2019',
     points: [
-      'Performed physical therapy evaluations, educated patients and/or caregivers on findings, created plans of care, and provided care',
-      'Conducted free injury screens and discussed injury prevention for runners training for the Boston Marathon',
-      'Worked closely with administrative staff to ensure insurance coverage of services for each patient and performed written and/or verbal appeals when services were denied by insurance in order to ensure that insurance covered the care they needed',
+      'Conducted evaluations, educated patients and/or caregivers on findings, created plans of care, and provided care',
+      'Worked closely with administrative staff to ensure insurance coverage of services for patients and performed written and/or verbal appeals when services were denied',
     ],
   },
   {
@@ -40,10 +40,10 @@ const positions = [
     link: 'https://damanyhealth.com/',
     daterange: 'May 2015 - December 2017',
     points: [
-      'Performed evaluations and treatments in a holistic manner on patients of all ages who traveled to the clinic from around the world with a wide array of orthopedic and/or neurological conditions, both acute and chronic',
-      'Authored an article in an occasional series about repetitive stress injury, published in ADVANCE for Physical Therapy magazine',
-      'Developed a treatment manual and continuing education coursework to teach specific treatment techniques developed by a colleague',
-      'Held the role of clinical instructor to a physical therapy student for an 8-week full-time clinical experience',
+      'Conducted evaluations and treatments in a holistic manner on patients of all ages who traveled to the clinic from around the world with a wide array of orthopedic and/or neurological conditions, both acute and chronic',
+      'Authored an article about repetitive stress injury, published in ADVANCE for Physical Therapy magazine',
+      'Developed a treatment manual and continuing education coursework to teach treatment techniques',
+      'Held the role of clinical instructor to a physical therapy student for a full-time clinical experience',
     ],
   },
 ];
