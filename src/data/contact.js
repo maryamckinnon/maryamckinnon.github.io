e6719ce9f24e5f920750099377a6322d203612e4
Mary McKinnon <your@email.heremaryamckinnon@gmail.com>
2022-08-22 15:16:11 -0700

    Changed github link to gitlab

--- src/data/contact.js
+++ src/data/contact.js
@@ -1,4 +1,4 @@
-import { faGithub } from '@fortawesome/free-brands-svg-icons/faGithub';
+import { faGitlab } from '@fortawesome/free-brands/svg-icons/faGitlab';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons/faLinkedinIn';
 import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 // See https://fontawesome.com/icons?d=gallery&s=brands,regular&m=free
@@ -6,9 +6,9 @@ import { faEnvelope } from '@fortawesome/free-regular-svg-icons/faEnvelope';
 
 const data = [
   {
-    link: 'https://github.com/maryamckinnon',
-    label: 'Github',
-    icon: faGithub,
+    link: 'https://gitlab.com/maryamckinnon',
+    label: 'Gitlab',
+    icon: faGitlab,
   },
   {
     link: 'https://www.linkedin.com/in/mary-mckinnon/',
