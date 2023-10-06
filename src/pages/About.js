7cfe87830e18210381a24e1d424966f63b4f4a2d
unknown <maryamckinnon@gmail.com>
2023-10-05 21:44:51 -0700

    Update data

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,15 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import ReactMarkdown from 'react-markdown';
-import raw from 'raw.macro';
 
 import Main from '../layouts/Main';
-
-// uses babel to load contents of file
-const markdown = raw('../data/about.md');
-
-// Make all hrefs react router links
-const LinkRenderer = ({ ...children }) => <Link {...children} />;
 
 const About = () => (
   <Main
@@ -22,13 +14,47 @@ const About = () => (
           <h2 data-testid="heading"><Link to="/about">About Me</Link></h2>
         </div>
       </header>
-      <ReactMarkdown
-        source={markdown}
-        renderers={{
-          Link: LinkRenderer,
-        }}
-        escapeHtml={false}
-      />
+      <h1>Intro</h1>
+      <p>
+        I am a full-stack software engineer, currently working at a
+        social health/ad tech company. My professional journey has
+        been a blend of diverse experiences, as I was a physical therapist
+        prior to transitioning into software engineering. As a curious and driven person,
+        I enjoy delving into the inner workings of complex systems, a trait
+        that drew me naturally to software engineering. This innate passion
+        for understanding and problem-solving fuels my enthusiasm to continuously
+        learn and grow within the tech space.
+      </p>
+      <br />
+      <p>
+        My transition to software engineering is underpinned by a deep-seated commitment
+        to driving positive change on a larger scale. I believe that technology holds the
+        key to revolutionizing the realms of health, wellness, and social impact. This
+        belief keeps me excited about the endless possibilities that software engineering
+        brings to these fields.
+      </p>
+      <h1>When not coding, I enjoy</h1>
+      <ul>
+        <li>hiking</li>
+        <li>meditation</li>
+        <li>long walks exploring San Francisco</li>
+        <li>
+          puzzles of any kind (some favorites are Wordle/Octordle, jigsaw puzzles,
+          and Soduko)
+        </li>
+        <li>cycling</li>
+        <li>being near large bodies of water</li>
+        <li>
+          listening to podcasts (Hidden Brain, We Can Do Hard Things, The Heal
+          Podcast, Radiolab)
+        </li>
+      </ul>
+      <h1>I dream of</h1>
+      <ul>
+        <li>having  positive impact on the world.</li>
+        <li>being a better version of myself daily.</li>
+        <li>continuing my journey of lifelong learning.</li>
+      </ul>
     </article>
   </Main>
 );
